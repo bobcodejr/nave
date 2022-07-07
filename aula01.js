0d5e357ea6a18107ecbf2b24adab9f6af429f80b
pborn84 <87150258+pborn84@users.noreply.github.com>
2022-07-07 11:44:41 -0300

    Aula 01

---
+++ aula01.js
@@ -0,0 +1,8 @@
+/*LÓGICA DE PROGRAMAÇÃO EM JAVASCRIPT*/
+/*comandos de saida*/
+
+let nome =  "Bobby Sinclair";
+
+
+console.log("Olá programador: " + nome);
+console.log(`Olá programador: ${nome}`);
