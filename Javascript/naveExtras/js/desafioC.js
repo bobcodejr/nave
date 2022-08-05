d59fe849638888a388120406a9b795a79f05b50d
bobcodejr <108553761+bobcodejr@users.noreply.github.com>
2022-08-05 17:11:19 -0300

    Aula
    de sexta-feira

---
+++ Javascript/naveExtras/js/desafioC.js
@@ -0,0 +1,18 @@
+/*Faça um programa para um caixa que receba quantos produtos voce quiser registrar, cada produto
+terá: nome, valor e "se tem desconto". Os produtos com desconto receberam 10% no pagamento
+em dinheiro e 5% no pagamento em cartão. Após as compras o total da compra será deduzido do valor pago 
+e exibiremos o troco na tela. */
+
+alert("Em um conjunto de números qual eh o primeiro numero par sorteado");
+
+let pagamento = 0
+
+for(i = 0; i <= 10; i++){
+
+
+
+
+
+
+
+}
